f4e315b6084f01f9c2ad71186652dd3432b99e68
Jonathan Markland <jonathan.markland@btinternet.com>
2020-10-30 14:00:27 +0000

    Maze filter

---
+++ FsAnyGame/MazeFilter.fs
@@ -0,0 +1,81 @@
+﻿module MazeFilter
+
+//   ########
+//   # #    #
+//   # # ####
+//   # # #  #
+//   ########
+
+
+
+[<Struct>]
+type DirectionMasks =
+    {
+        UpMask    : byte
+        DownMask  : byte
+        LeftMask  : byte
+        RightMask : byte
+    }
+
+
+let NewDirectionMasks u d l r =
+    {
+        UpMask    = u
+        DownMask  = d
+        LeftMask  = l
+        RightMask = r
+    }
+
+
+/// Maze parser general algorithm.
+/// Could be used to deduce four-way "box drawing" characters from 
+/// a plain text matrix using a single character eg: # as a wall marker.
+/// The input maze type is user-definable.  The output is a byte array
+/// with user-definable bit values for the direction bits.
+let MazeFilterIter isWall width height masks action =
+
+    let test x y mask =
+        if x >= 0 && y >= 0 && x < width && y < height then
+            if isWall x y then mask else 0uy
+        else
+            0uy
+
+    for y in 0..(height-1) do
+        for x in 0..(width-1) do
+            if isWall x y then
+                let u = test x (y-1) masks.UpMask
+                let d = test x (y+1) masks.DownMask
+                let l = test (x-1) y masks.LeftMask
+                let r = test (x+1) y masks.RightMask
+                action x y (u ||| d ||| l ||| r)
+            else
+                action x y 0uy  // To completely define the output
+
+
+
+/// Parse array of equal-length strings and return a row-primary
+/// byte array of direction bitmasks, starting in the top left corner.
+/// All rows must be the same width for this to return a value.
+let MazeByteArray isWallChar (maze:string[]) =
+
+    let height = maze.Length
+    if height > 0 then
+
+        let width = maze.[0].Length
+        if width > 0 && maze |> Array.forall (fun str -> str.Length = width) then
+
+            let isWall x y = isWallChar maze.[y].[x]
+            let masks      = NewDirectionMasks 1uy 2uy 4uy 8uy
+            let height     = maze.Length
+
+            let output = Array.zeroCreate<byte> (width * height)
+            let action x y mask = output.[y * width + x] <- mask
+
+            MazeFilterIter isWall width height masks action
+
+            Some output
+
+        else None
+
+    else None
+
---
+++ FsAnyGameTests/MazeFilterTests.fs
@@ -0,0 +1,189 @@
+﻿module MazeFilterTests
+
+open Xunit
+open FsXunit
+open MazeFilter
+
+
+// --------------------------------------------------------------------------------------------------------
+//  Test support
+// --------------------------------------------------------------------------------------------------------
+
+let ToUnicodeBoxDrawing width (output:byte[]) =
+
+    let unicodeBoxDrawing = " ╵╷│╴┘┐┤╶└┌├─┴┬┼"
+
+    let boxDrawOutput = 
+        output 
+            |> Array.map (fun n -> unicodeBoxDrawing.[int n]) 
+            |> Array.chunkBySize width
+            |> Array.toList
+            |> List.map (fun arr -> new System.String(arr))
+
+    boxDrawOutput
+
+
+
+let ToFSharpBoxDrawSourceCodeSeq width output =
+
+    let boxDraw = ToUnicodeBoxDrawing width output
+
+    seq {
+        yield "let expected ="
+        yield "    ["
+        yield! boxDraw |> List.map (fun str -> sprintf "        \"%s\"" str)
+        yield "    ]"
+    }
+
+
+
+let MazeByteArrayUsingHashAsWall = 
+    MazeByteArray (fun ch -> ch = '#')
+
+
+
+let MazeToFSharpBoxDrawSourceCode (maze:string[]) =
+
+    let width = maze.[0].Length
+
+    maze |> MazeByteArrayUsingHashAsWall
+         |> Option.map (fun maze -> maze |> ToFSharpBoxDrawSourceCodeSeq width |> String.concat "\r\n")
+
+          
+
+
+
+// --------------------------------------------------------------------------------------------------------
+//  Test
+// --------------------------------------------------------------------------------------------------------
+
+let WhenTransformed (maze:string[]) =
+
+    let width = if maze.Length > 0 then maze.[0].Length else 0
+    maze 
+        |> MazeByteArrayUsingHashAsWall
+        |> Option.map (ToUnicodeBoxDrawing width)
+
+
+
+// --------------------------------------------------------------------------------------------------------
+//  MazeByteArray basic edge cases
+// --------------------------------------------------------------------------------------------------------
+        
+[<Fact>]
+let ``Empty input to MazeByteArray yields None`` () =
+    [||] |> MazeByteArrayUsingHashAsWall |> ShouldEqual None
+
+[<Fact>]
+let ``Array of one empty string input to MazeByteArray yields None`` () =
+    [| "" |] |> MazeByteArrayUsingHashAsWall |> ShouldEqual None
+
+[<Fact>]
+let ``Array of two empty string inputs to MazeByteArray yields None`` () =
+    [| "" ; "" |] |> MazeByteArrayUsingHashAsWall |> ShouldEqual None
+
+
+
+// --------------------------------------------------------------------------------------------------------
+//  Standard maze filter
+// --------------------------------------------------------------------------------------------------------
+
+[<Fact>]
+let ``MazeByteArray applied to basic maze produces correct pieces viewed via box drawing`` () =
+
+    let maze =
+        [|
+            "########"
+            "# #    #"
+            "# # ## #"
+            "# #  # #"
+            "# #### #"
+            "#      #"
+            "########"
+        |]
+
+    let expected =
+        [
+            "┌─┬────┐"
+            "│ │    │"
+            "│ │ ╶┐ │"
+            "│ │  │ │"
+            "│ └──┘ │"
+            "│      │"
+            "└──────┘"
+        ]
+
+    maze |> WhenTransformed |> ShouldEqual (Some expected)
+
+// --------------------------------------------------------------------------------------------------------
+
+[<Fact>]
+let ``MazeByteArray applied to comprehensive case maze produces correct pieces viewed via box drawing`` () =
+
+    let maze =
+        [|
+            "###############"
+            "#             #"
+            "# ########### #"
+            "# #   # #   # #"
+            "# ##  #    ## #"
+            "# #         # #"
+            "# ###     ### #"
+            "# #    #    # #"
+            "# #         # #"
+            "# #   #   ### #"
+            "# # # #   ### #"
+            "# ########### #"
+            "#             #"
+            "###############"
+        |]
+
+    // Reminder of how to obtain F# source code:  
+    // let fssrc = maze |> MazeToFSharpBoxDrawSourceCode
+
+    let expected =
+        [
+            "┌─────────────┐"
+            "│             │"
+            "│ ┌───┬─┬───┐ │"
+            "│ │   │ ╵   │ │"
+            "│ ├╴  ╵    ╶┤ │"
+            "│ │         │ │"
+            "│ ├─╴     ╶─┤ │"
+            "│ │         │ │"
+            "│ │         │ │"
+            "│ │   ╷   ┌┬┤ │"
+            "│ │ ╷ │   ├┼┤ │"
+            "│ └─┴─┴───┴┴┘ │"
+            "│             │"
+            "└─────────────┘"
+        ]
+
+    maze |> WhenTransformed |> ShouldEqual (Some expected)
+
+// --------------------------------------------------------------------------------------------------------
+//  Edge cases
+// --------------------------------------------------------------------------------------------------------
+
+[<Fact>]
+let ``The 0 x 0 maze`` () =
+
+    [||] |> WhenTransformed |> ShouldEqual None
+
+
+
+[<Fact>]
+let ``The 0 x 5 maze`` () =
+
+    let maze = 
+        [|
+            ""
+            ""
+            ""
+            ""
+            ""
+        |]
+
+    maze |> WhenTransformed |> ShouldEqual None
+
+
