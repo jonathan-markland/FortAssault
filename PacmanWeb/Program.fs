3b68c9b8622fb6bee35bd2a7e12e4938a4fefc10
Jonathan Markland <jonathan.markland@btinternet.com>
2020-11-22 22:37:17 +0000

    PacmanWeb project added

---
+++ PacmanWeb/Program.fs
@@ -0,0 +1,43 @@
+﻿module PacmanWeb
+
+open WebGameFramework
+
+open PacmanImageFiles
+open Storyboard
+open Input
+
+
+
+// ------------------------------------------------------------------------------------------------------------
+//  BOOT
+// ------------------------------------------------------------------------------------------------------------
+
+let WebMain () =
+
+    let pacmanKeysNeeded =
+        [
+            WebBrowserKeyCode 37
+            WebBrowserKeyCode 39
+            WebBrowserKeyCode 38
+            WebBrowserKeyCode 40
+            WebBrowserKeyCode 90
+        ]
+
+    let stubGlobalStateConstructor () = Ok 1  // Feature not used by Pacman.
+
+    LoadResourceFilesThenDo 
+        PacmanResourceImages 
+        PacmanFontResourceImages 
+        (FrameworkWebMain
+            pacmanKeysNeeded
+            stubGlobalStateConstructor 
+            NewPacmanStoryboard)
+
+
+
+// ------------------------------------------------------------------------------------------------------------
+//  Javascript-land runs this script right away:
+// ------------------------------------------------------------------------------------------------------------
+
+WebMain ()
+
