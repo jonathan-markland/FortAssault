ea720bb28a436bce34b033172bd389691fa0f166
Jonathan Markland <jonathan.markland@btinternet.com>
2020-11-28 22:20:08 +0000

    Batch files for performing production build, and path fix.

--- FsFortAssaultWeb/webpack.config.js
+++ FsFortAssaultWeb/webpack.config.js
@@ -6,7 +6,7 @@ var path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./FortAssaultWeb.fsproj",
+    entry: "./FsFortAssaultWeb.fsproj",
     output: {
         path: path.join(__dirname, "./wwwroot"),
         filename: "bundle.js",
