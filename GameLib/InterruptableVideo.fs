8dc6b48c334f09274e115d25355fb417bcfbc401
Jonathan Markland <jonathan.markland@btinternet.com>
2021-02-28 11:36:49 +0000

    Interruptible video can be re-written in terms of Sustain mode

--- GameLib/InterruptableVideo.fs
+++ GameLib/InterruptableVideo.fs
@@ -1,47 +1,15 @@
-﻿/// TODO: Re-write in terms of the general purpose "SustainModeUntil" filter.
-module InterruptableVideo
+﻿module InterruptableVideo
 
-open Time
-open GameStateManagement
+open SustainModeUntil
 open Input
-
-
-
-type private InterruptableVideoModel =
-    {
-        InterruptKey       : WebBrowserKeyCode
-        VideoPlayerState   : ErasedGameState
-        WhereToOnInterrupt : float32<seconds> -> ErasedGameState
-    }
-
-
-
-let private RenderInterruptable render model (gameTime:float32<seconds>) =
-    
-    model.VideoPlayerState.Draw render gameTime
-
-
-
-let private NextInterruptableState gameState (keyStateGetter:WebBrowserKeyCode -> InputEventKeyState) gameTime elapsed =
-
-    let model = ModelFrom gameState
-
-    if (keyStateGetter model.InterruptKey).JustDown then
-        model.WhereToOnInterrupt gameTime
-    else
-        gameState |> WithUpdatedModel 
-            { model with VideoPlayerState = model.VideoPlayerState.Frame KeyStatesInhibited gameTime elapsed }
-        
 
 
 let AsInterruptableVideoThen whereToAfter interruptKey ongoingVideo =
 
-    let interruptableVideoModel =
-        {
-            InterruptKey       = interruptKey
-            VideoPlayerState   = ongoingVideo
-            WhereToOnInterrupt = whereToAfter
-        }
-
-    NewGameState NextInterruptableState RenderInterruptable interruptableVideoModel
+    ongoingVideo |> SustainModeUntil (fun gameTime keyStateGetter ->
+        if (keyStateGetter interruptKey).JustDown then
+            Some (whereToAfter gameTime)
+        else
+            None
+    )
 
